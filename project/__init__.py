7181e9508dd41cedcb15b6f11f4b12693d00271b
estherjsuh <estherjsuh@gmail.com>
2020-04-14 13:53:56 -0700

    added sqlalchemy and postgress

--- project/__init__.py
+++ project/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 #If we set instance_relative_config=True when we create our app with the Flask() call, app.config.from_pyfile() will load the specified file from the instance/ directory.
 
 ##CONFIG - LOADS CONFIGURATION FROM CONFIG FILE##
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('flask.cfg')
+
+db = SQLAlchemy(app)
 
 ##BLUEPRINTS##
 
---
+++ project/plants/__init__.py
@@ -0,0 +1,1 @@
+#even though this is an empty file, python will know that this is a package
