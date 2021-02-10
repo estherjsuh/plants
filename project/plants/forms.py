bafc084608172db8e6830d8a93e7e8eef3e2f644
estherjsuh <estherjsuh@gmail.com>
2021-02-10 14:56:01 -0800

    Clean forms.py with pylint, 10/10

--- project/plants/forms.py
+++ project/plants/forms.py
@@ -1,16 +1,22 @@
+'''
+2 Forms to handle plant data
+'''
+
 from flask_wtf import FlaskForm
-from wtforms import Form, StringField, IntegerField
+from flask_wtf.file import FileField, FileRequired
+from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired
-from flask_wtf.file import FileField, FileAllowed, FileRequired
 
 
 class AddPlantForm(FlaskForm):
+    '''Adds new plant'''
     plant_name = StringField('Plant Name', validators=[DataRequired()])
     plant_description = StringField('Plant Description', validators=[DataRequired()])
     watering_frequency = IntegerField('Watering Frequency', validators=[DataRequired()])
     plant_photo = FileField('Plant Photo', validators=[FileRequired()])
 
 class EditPlantForm(FlaskForm):
+    '''Edits exisitng plant'''
     plant_name  = StringField('Plant Name', validators=[])
     plant_description = StringField('Plant Description', validators=[])
     watering_frequency = IntegerField('Watering Frequency', validators=[])
